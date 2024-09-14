d6580a8a881bab9dd1d94b0d7f8269a7c644d7a4
FloRide1 <florian.reimat@orange.fr>
2024-09-14 18:14:41 +0200

    feat(env): Add BACKEND_URL env loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import nightwatchPlugin from "vite-plugin-nightwatch";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue(), nightwatchPlugin(), vueDevTools()],
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        plugins: [vue(), nightwatchPlugin(), vueDevTools()],
+        define: {
+            __BACKEND_URL__: JSON.stringify(env.BACKEND_URL),
         },
-    },
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
+    };
 });
