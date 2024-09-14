7dc643df272ac24a873c8ea602bbe44d9ea276d0
FloRide1 <florian.reimat@orange.fr>
2024-09-14 18:43:00 +0200

    feat(style): Add Vuetify components library

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import { aliases, fa } from "vuetify/iconsets/fa";
+import "@fortawesome/fontawesome-free/css/all.css";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
 import App from "./App.vue";
 import router from "./router";
 import axios from "./plugins/axios";
 
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: { defaultTheme: "dark" },
+    icons: {
+        defaultSet: "fa",
+        aliases,
+        sets: {
+            fa,
+        },
+    },
+});
+
 const app = createApp(App);
 
+app.use(vuetify);
 app.use(createPinia());
 app.use(router);
 app.use(axios, {
