10b21dd7f4e78f2d9e76ed3d4df7be94e11002e2
FloRide1 <florian.reimat@orange.fr>
2024-09-17 19:37:02 +0200

    feat(app): Add basic login app

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,118 @@
-<script setup lang="ts"></script>
+<template>
+    <v-app>
+        <div v-if="serverIsUp">
+            <v-app-bar>
+                <template v-slot:prepend>
+                    <v-img aspect-ratio="1/1" width="80" cover src="@/assets/logo.svg">
+                        <template v-slot:placeholder>
+                            <v-skeleton-loader
+                                class="d-flex align-center justify-center fill-height"
+                                type="list-item-avatar"
+                            ></v-skeleton-loader>
+                        </template>
+                    </v-img>
+                </template>
+                <template v-slot:append>
+                    <v-btn
+                        v-if="!userStore.connected"
+                        @click="login"
+                        variant="tonal"
+                        prepend-icon="fa-brands fa-microsoft"
+                        :loading="loginLoading"
+                        :disabled="loginLoading"
+                        icon="fa-solid fa-right-to-bracket"
+                    >
+                    </v-btn>
+                    <v-avatar v-else color="blue">
+                        {{ nameInitials }}
+                    </v-avatar>
+                </template>
+            </v-app-bar>
+        </div>
+        <RouterView />
+    </v-app>
+</template>
 
-<template></template>
+<script lang="ts">
+import type { AxiosInstance } from "axios";
+import { useUserStore } from "@/stores/user";
+import type { User } from "@/types/User";
+import type { Router } from "vue-router";
+
+declare module "@vue/runtime-core" {
+    interface ComponentCustomProperties {
+        $axios: AxiosInstance;
+        $backendUrl: string;
+        $router: Router;
+    }
+}
+
+export default {
+    data() {
+        return {
+            serverIsUp: true,
+            loginLoading: false,
+            userStore: useUserStore(),
+        };
+    },
+    methods: {
+        isBackendUp() {
+            this.$axios
+                .get("/status")
+                .then((res) => {
+                    if (res.status != 200 || res.data != "UP") {
+                        this.serverIsUp = false;
+                        this.$router.push("/unavailable");
+                    }
+                })
+                .catch((_err) => {
+                    this.serverIsUp = false;
+                    this.$router.push("/unavailable");
+                });
+        },
+        isLoggedIn() {
+            this.$axios
+                .get<User>("/me")
+                .then((res) => {
+                    if (res.status == 200) {
+                        this.userStore.connected = true;
+                        this.userStore.username = res.data.username;
+                        this.userStore.name = res.data.name;
+                        this.userStore.email = res.data.email;
+                        this.userStore.id = res.data.id;
+                    } else {
+                        this.userStore.connected = false;
+                        this.userStore.username = undefined;
+                        this.userStore.name = undefined;
+                        this.userStore.email = undefined;
+                        this.userStore.id = undefined;
+                    }
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        },
+        async login() {
+            this.loginLoading = true;
+            let res = await this.$axios.get("/me");
+            if (res.status != 200) {
+                window.location.href = `${this.$backendUrl}/login`;
+            }
+            this.loginLoading = false;
+        },
+    },
+    computed: {
+        nameInitials() {
+            return this.userStore.name
+                ?.split(" ")
+                .map((w) => w[0])
+                .join("")
+                .toUpperCase();
+        },
+    },
+    mounted() {
+        this.isBackendUp();
+        this.isLoggedIn();
+    },
+};
+</script>
