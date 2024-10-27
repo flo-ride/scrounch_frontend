297fd2eb77510668248981142f4befb573a5acc8
flo-ride <43076999+flo-ride@users.noreply.github.com>
2024-10-27 16:25:10 +0100

    feat: Add Avatar Menu + Logout

--- src/components/MainToolbar.vue
+++ src/components/MainToolbar.vue
@@ -25,7 +25,6 @@
             </RouterLink>
         </template>
         <template v-slot:append>
-            <v-btn v-if="userStore.is_admin" @click="$router.push('/admin')"> Admin </v-btn>
             <v-btn icon="fa-solid fa-language" @click="toggleLanguage"> </v-btn>
             <v-btn
                 :icon="themeName == `dark` ? `fa-solid fa-sun` : `fa-solid fa-moon`"
@@ -42,9 +41,32 @@
                 icon="fa-solid fa-right-to-bracket"
             >
             </v-btn>
-            <v-avatar v-else color="blue">
-                {{ nameInitials }}
-            </v-avatar>
+            <v-menu v-else open-on-hover transition="slide-y-transition">
+                <template v-slot:activator="{ props }">
+                    <v-avatar color="blue" v-bind="props">
+                        {{ nameInitials }}
+                    </v-avatar>
+                </template>
+                <v-list>
+                    <v-list-item @click="">
+                        <v-icon> fa-solid fa-user </v-icon>
+                        {{ $t("toolbar.account") }}
+                    </v-list-item>
+                    <v-list-item @click="">
+                        <v-icon> fa-solid fa-file-invoice </v-icon>
+                        {{ $t("toolbar.previous") }}
+                    </v-list-item>
+                    <v-list-item v-if="userStore.is_admin" @click="$router.push('/admin')">
+                        <v-icon> fa-solid fa-gear </v-icon>
+                        {{ $t("toolbar.admin") }}
+                    </v-list-item>
+                    <v-divider></v-divider>
+                    <v-list-item @click="logout">
+                        <v-icon> fa-solid fa-arrow-right-from-bracket </v-icon>
+                        {{ $t("toolbar.logout") }}
+                    </v-list-item>
+                </v-list>
+            </v-menu>
         </template>
     </v-app-bar>
 </template>
@@ -72,6 +94,14 @@ export default defineComponent({
                 window.location.href = `${this.$backendUrl}/login`;
             }
             this.loginLoading = false;
+        },
+        async logout() {
+            // @ts-ignore
+            let res = await this.$axios.get("/me");
+            if (res.status == 200) {
+                // @ts-ignore
+                window.location.href = `${this.$backendUrl}/logout`;
+            }
         },
 
         toggleLanguage() {
